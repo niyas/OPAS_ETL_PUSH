2de05dd9e0a7b983e860eb301ca7e8cd513a9939
Mohammed Niyas <Mohammed Niyas>
2018-01-23 13:10:17 +0000

    get email fields from the config table

--- OPAS_ETL_PUSH/Program.cs
+++ OPAS_ETL_PUSH/Program.cs
@@ -101,13 +101,28 @@ namespace OPAS_ETL_PUSH
         {
             DAL objDal = new DAL();
             DataTable dt = objDal.ExecuteDataSet("USP_SelectDistinctAssignee").Tables[0];
+            DataTable dtConfig = getConfig("updateIMS");
+            string applicationUrl = ConfigurationManager.AppSettings["automationToolUrl"].ToString();
+            var query = from d in dtConfig.AsEnumerable()
+                        where d.Field<string>("Key").Contains("InitialNotice") || d.Field<string>("Key") == "cc"
+                        || d.Field<string>("Key") == "bcc" || d.Field<string>("Key") == "to"
+                        select d;
+            DataTable dtConfigReminder = query.CopyToDataTable<DataRow>();
             string baseUrl = ConfigurationManager.AppSettings["automationToolUrl"].ToString();
 
             foreach (DataRow dr in dt.Rows)
             {
                 string url = baseUrl + dr[0].ToString().Replace(" ", "");
-                sendEmail(url, dr[1].ToString(), dr[0].ToString());
+                sendEmail(url, dtConfigReminder);
             }
+        }
+
+        private static DataTable getConfig(string type)
+        {
+            DAL objDal = new DAL();
+            objDal.AddParameter("@type", type);
+            DataTable dt = objDal.ExecuteDataSet("usp_getConfigDetails").Tables[0];
+            return dt;
         }
 
         /**
@@ -118,21 +133,81 @@ namespace OPAS_ETL_PUSH
          * @params {String} name
          * @returns {Void}
          */
-        private static void sendEmail(string url, string email, string name)
+        private static void sendEmail(string url, DataTable config)
         {
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient();
-            mail.From = new MailAddress("niyasatwork@gmail.com");
-            mail.To.Add(email);
-            mail.Subject = "Ticket Automation Email";
-            mail.Body = "Hi " + name + ",<br/> Please update your tickets using the following link below. <br/>" + url;
-            mail.IsBodyHtml = true;    
-            SmtpServer.Host = "smtp.gmail.com";
-            SmtpServer.Port = 25;
-            SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("niyasatwork@gmail.com", "YourPassword");
-            SmtpServer.UseDefaultCredentials = false;
-            SmtpServer.Send(mail);
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient();
+                string[] cclist = { },
+                         tolist = { },
+                         bcclist = { };
+
+                string subject = "",
+                        body = "";
+
+                foreach (DataRow dr in config.Rows)
+                {
+                    if (dr["Key"].ToString().ToLower().Trim() == "to")
+                        tolist = dr["Value"].ToString().Split(',');
+
+                    else if (dr["Key"].ToString().ToLower().Trim() == "cc")
+                        cclist = dr["Value"].ToString().Split(',');
+
+                    else if (dr["Key"].ToString().ToLower().Trim() == "bcc")
+                        bcclist = dr["Value"].ToString().Split(',');
+
+                    else if (dr["Key"].ToString().ToLower().Contains("body"))
+                        body = dr["Value"].ToString();
+
+                    else if (dr["Key"].ToString().ToLower().Contains("subject"))
+                        subject = dr["Value"].ToString();
+                }
+
+                if (tolist.Count() > 0)
+                {
+                    foreach (string tos in tolist)
+                    {
+                        mail.To.Add(tos);
+                    }
+                }
+
+                if (cclist.Count() > 0)
+                {
+                    foreach (string cc in cclist)
+                    {
+                        mail.CC.Add(cc);
+                    }
+                }
+
+                if (bcclist.Count() > 0)
+                {
+                    foreach (string bcc in cclist)
+                    {
+                        mail.Bcc.Add(bcc);
+                    }
+                }
+
+                body = body.Replace("@applicationUrl", url);
+
+                mail.From = new MailAddress("niyasatwork@gmail.com");
+
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                SmtpServer.Host = "smtp.gmail.com";
+                SmtpServer.Port = 587;
+                SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("niyasatwork@gmail.com", "yourpassword");
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
         }
     }
 }
